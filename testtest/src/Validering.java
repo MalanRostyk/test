07c62412d4567b01d6713f372de8dae0897fc3e0
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-05-08 14:14:08 +0200

    n

--- testtest/src/Validering.java
+++ testtest/src/Validering.java
@@ -2,6 +2,7 @@
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import oru.inf.InfDB;
+import oru.inf.InfException;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,24 +13,30 @@ import oru.inf.InfDB;
  *
  * @author rostykmalanchuk
  */
+
 public class Validering {
-    private InfDB db;
+    private static InfDB db;
+    
     public static boolean bokstavsKontroll(JTextField inmatning){
-    boolean resultat = true;
-    try{
+        boolean resultat = true;
         if(inmatning.getText().isEmpty()){
             JOptionPane.showMessageDialog(null, "Rutan är tom!");
-        }else{
-            String id = inmatning.getText();
-            String sqlFraga = "slelct Namn from medlem where Medlemsnummer = "+id;
-            String svar = db.fetchSingle(sqlFraga);
-            
+            resultat = false;
+            inmatning.requestFocus();
         }
-    }catch(){
-        
+        return resultat;
     }
-    
-    
-    return resultat;
-}
+    public static boolean isHeltal(JTextField ruta){
+        boolean kontroll = true;
+        try{
+            String inString = ruta.getText();
+            Integer.parseInt(inString);
+            ruta.requestFocus();
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Du matade in bokstäver, testa ange ett helttal!");
+            kontroll = false;
+            ruta.requestFocus();
+        }
+        return kontroll;
+    }
 }
