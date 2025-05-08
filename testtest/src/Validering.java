f4da105503d2da5fa667c52117f1223dedeee9de
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-05-08 13:40:28 +0200

    Update Validering.java

--- testtest/src/Validering.java
+++ testtest/src/Validering.java
@@ -1,3 +1,8 @@
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import oru.inf.InfDB;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,9 +13,21 @@
  * @author rostykmalanchuk
  */
 public class Validering {
-    public static boolean bokstavsKontroll(){
+    private InfDB db;
+    public static boolean bokstavsKontroll(JTextField inmatning){
     boolean resultat = true;
-    
+    try{
+        if(inmatning.getText().isEmpty()){
+            JOptionPane.showMessageDialog(null, "Rutan är tom!");
+        }else{
+            String id = inmatning.getText();
+            String sqlFraga = "slelct Namn from medlem where Medlemsnummer = "+id;
+            String svar = db.fetchSingle(sqlFraga);
+            
+        }
+    }catch(){
+        
+    }
     
     
     return resultat;
